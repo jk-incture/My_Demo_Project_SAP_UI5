60697eab1e10b077c25fb67bf98a108f8a79be49
Jyotiranjan Kunda <jyotiranjan.kunda@incture.com>
2024-03-11 09:24:20 +0000

    Edit some files

--- webapp/controller/Second.controller.js
+++ webapp/controller/Second.controller.js
@@ -1,25 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/m/Dialog",
+    "sap/m/Button",
+    "sap/m/Text"
+], (Controller, Dialog, Button, Text) => {
     "use strict";
 
     return Controller.extend("com.incture.mydemoproject1.controller.Second", {
-        onInit: function() {
-            
-        }, 
+        onInit: function () {
 
-        onBack: function(){
+        },
+
+        onBack: function () {
             // var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             // oRouter.navTo("RouteDemoView");
 
             this.getOwnerComponent().getRouter().navTo("RouteDemoView");
         },
 
-        onSubmit: function(){
+        onSubmit: function () {
             // Setting name dynamically
             var name = this.getView().byId('idIpName').getValue();
-            var msg = 'Hi! Welcome '+ name;
+            var msg = 'Hi! Welcome ' + name;
             this.getView().byId('idTxtMsg').setText(msg);
 
             // Setting alignment dynamically
@@ -31,11 +33,11 @@ sap.ui.define([
             this.getView().byId('idIpAlign').setEnabled(false);
         },
 
-        onHideShow: function(){
+        onHideShow: function () {
             // debugger;
             var btnText = this.getView().byId('idBtnHideShow').getText();
 
-            if(btnText == 'Hide'){
+            if (btnText == 'Hide') {
                 this.getView().byId('idTxtMsg').setVisible(false);
                 this.getView().byId('idLblName').setVisible(false);
                 this.getView().byId('idIpName').setVisible(false);
@@ -44,7 +46,7 @@ sap.ui.define([
 
                 this.getView().byId('idBtnHideShow').setText('Show');
             }
-            else{
+            else {
                 this.getView().byId('idTxtMsg').setVisible(true);
                 this.getView().byId('idLblName').setVisible(true);
                 this.getView().byId('idIpName').setVisible(true);
@@ -53,6 +55,62 @@ sap.ui.define([
 
                 this.getView().byId('idBtnHideShow').setText('Hide');
             }
+        },
+
+        onDefaultDialog: function () {
+            if (!this.oDefaultDialog) {
+                this.oDefaultDialog = new Dialog({
+                    title: "Hello Dialog",
+                    content: new Text({
+                        text: "Hi, I am a Dialog"
+                    }),
+                    beginButton: new Button({
+                        // type: ButtonType.Emphasized,
+                        text: "OK",
+                        press: function () {
+                            this.oDefaultDialog.close();
+                        }.bind(this)
+                    }),
+                    endButton: new Button({
+                        // type: ButtonType.Default,
+                        text: "Close",
+                        press: function () {
+                            this.oDefaultDialog.close()
+                        }.bind(this)
+                    })
+                });
+
+                this.getView().addDependent(this.oDefaultDialog);
+            }
+            this.oDefaultDialog.open();
+        },
+
+
+        onOpenDialog() {
+            // create dialog lazily
+            this.pDialog ??= this.loadFragment({
+                name: "com.incture.mydemoproject1.fragment.Login"
+            });
+
+            this.pDialog.then((oDialog) => oDialog.open()).catch((error) => console.log(error));
+        },
+
+        // Second method
+
+        /*
+        onOpenDialog() {
+            if(!this.pDialog){
+                this.pDialog = this.loadFragment({
+                    name: "com.incture.mydemoproject1.fragment.Login"
+                });
+            }
+        
+            this.pDialog.then((oDialog) => oDialog.open());
+        },
+        */
+
+        onCloseDialog(){
+            this.pDialog.then((oDialog) => oDialog.close());
         }
     })
 });
